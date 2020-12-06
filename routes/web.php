3ab18e55f82563c20308f58ae338e9dbd2cfef0a
unknown <ztilam60@gmail.com>
2020-12-06 13:29:20 +0700

    sdf

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]); 
+Auth::routes(['register' => false]);
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
